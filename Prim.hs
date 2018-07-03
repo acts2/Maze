b431936485e9a7d3dddae162bfdf0477d5be537a
Pedro Queiroga <psq@cin.ufpe.br>
2018-07-03 15:49:22 -0300

    iniciaGrafoConexo agora eh planar

--- Prim.hs
+++ Prim.hs
@@ -8,6 +8,7 @@ type Grafo = [ListaAdj]
 inicializaGrafoConexo :: Int -> Int -> Grafo
 inicializaGrafoConexo width height = listaAdj
   where
-    vertices = [(i, j) | i <- [0..width-1], j <- [0..height-1]]
-    listaAdj = [((i,j),vertices) | (i, j) <- vertices]
+    vertices  = [(i, j) | i <- [0..width-1], j <- [0..height-1]]
+    listaAdj  = [((i,j), verticesAdjacentes (i,j) width height) | (i, j) <- vertices]
 
+verticesAdjacentes vertice width height = map (\((i,x),(j,y)) -> (i+x, j+y)) $ filter (\((i,x),(j,y)) -> if (x+i >= height || y+j >= width || x+i < 0 || y+j < 0 || (x==y) || x+y == 0) then False else True) [((i,x), (j,y)) | let (i, j) = vertice, x <- [(-1), 0, 1], y <- [(-1), 0, 1]]
